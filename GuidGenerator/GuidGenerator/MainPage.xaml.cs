eaec7d4fdf5f65fab131bf7a9150b4cffb513e2c
sixeyed <es@sixeyed.com>
2010-05-13 18:31:19 +0100

    Initial add to SCM

---
+++ GuidGenerator/GuidGenerator/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using GuidGenerator.Cryptography;
+
+namespace GuidGenerator
+{
+    public partial class MainPage : UserControl
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGUID_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtInput.Text))
+            {
+                txtGUID.Text = Guid.NewGuid().ToString();
+            }
+            else
+            {
+                txtGUID.Text = GetDeterministicGuid(txtInput.Text).ToString();
+            }
+            Clipboard.SetText(txtGUID.Text);
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            txtInput.Text = txtGUID.Text = string.Empty;
+        }
+
+        private Guid GetDeterministicGuid(string input)
+        {
+            var inputBytes = Encoding.Unicode.GetBytes(input);
+            var hashBytes = MD5HashProvider.ComputeHash(inputBytes);
+            Guid hashGuid = new Guid(hashBytes);
+            return hashGuid;
+        }
+    }
+}
---
+++ GuidGenerator/GuidGenerator/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GuidGenerator")]
+[assembly: AssemblyCompany("Sixeyed Consulting")]
+[assembly: AssemblyProduct("GuidGenerator")]
+[assembly: AssemblyCopyright("Copyright © Sixeyed Consulting 2010")]
+[assembly: ComVisible(false)]
+[assembly: Guid("725566a8-8971-4272-aa3b-3e36060e5e84")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
